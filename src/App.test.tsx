e7a89079c4388d1f233faa2b05f09d1be84a642b
DM <mcnally@udel.edu>
2021-09-23 17:07:50 -0400

    very broken testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -25,4 +25,23 @@ describe('App', () => {
     const element = await screen.findByTestId("answer-label");
     expect(element).toBeInTheDocument();
   })
+// -------------------------------------------------------------------------------------------------------------------------
+  it("does not show the answer when swap current card button is clicked", async () => {
+    const button = screen.getByTestId("swap-current-card-button");
+    button.click();
+    const element = await screen.queryByTestId("answer-label"); // Query not expected
+    expect(element).not.toBeInTheDocument();
+  });
+
+  /*it("should shuffle when shuffle button clicked", async () => {
+    const button = screen.getByTestId("shuffle-button");
+    const og = JSON.stringify(screen.getByTestId("user-list"));
+    button.click();
+    const changed = await screen.findByTestId("user-list");
+    expect(JSON.stringify(changed)).not.toEqual(og);
+  });*/
+
+  it("should change the prompt when swap current card is clicked", async () => {
+    
+  });
 })
